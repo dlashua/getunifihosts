8c48f08f3493379d6714d572943a0a671b6c28a4
Daniel Lashua <daniel@inklog.net>
2020-09-15 13:26:54 -0500

    Add ".local" to hostnames

--- unifi.py
+++ unifi.py
@@ -35,7 +35,7 @@ def get_active_clients(session):
     return r.json()['data']
 
 def prettify(name):
-    return re.sub(r'[^a-zA-Z0-9-]', "", name)
+    return re.sub(r'[^a-zA-Z0-9-]', "", name) + ".local"
     #if re.search('^[a-zA-Z0-9-]+$', c['name'])
 
 
